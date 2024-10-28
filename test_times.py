e3985c2e5c119f2d4af3bacd2b2c9d2421a3c4c7
Zhexuan <ucapzso@ucl.ac.uk>
2024-10-28 19:00:13 +0800

    Add non-overlap time test

--- test_times.py
+++ test_times.py
@@ -1,4 +1,5 @@
 from times import compute_overlap_time, time_range
+from pytest import raises
 
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -6,4 +7,12 @@ def test_given_input():
     
     result = compute_overlap_time(large,short) 
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+def test_non_overlap():
+    large = time_range("2010-01-13 05:00:00", "2010-01-13 10:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    
+    result = compute_overlap_time(large,short) 
+    expected = []
     assert result == expected
