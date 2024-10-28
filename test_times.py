7fdc94316aaf77dac7c67eb034db939d71d3cf02
Zhexuan <ucapzso@ucl.ac.uk>
2024-10-28 19:02:18 +0800

    Add multinterval time test

--- test_times.py
+++ test_times.py
@@ -24,3 +24,11 @@ def test_just_touching():
     result = compute_overlap_time(large,short) 
     expected = ['2010-01-12 12:00:00']
     assert result == expected
+
+def test_multintervals_each():
+    large = time_range("2010-01-12 10:30:00", "2010-01-12 10:40:00",5, 25)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+
+    result = compute_overlap_time(large,short) 
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:31:40'), ('2010-01-12 10:32:05', '2010-01-12 10:33:45'), ('2010-01-12 10:34:10', '2010-01-12 10:35:50'), ('2010-01-12 10:36:15', '2010-01-12 10:37:00'), ('2010-01-12 10:38:20', '2010-01-12 10:40:00')]
+    assert result == expected
