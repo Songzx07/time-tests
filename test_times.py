e057e1a1ef3806f7da18c004e2c92bd1c8884440
Zhexuan <ucapzso@ucl.ac.uk>
2024-10-29 05:01:22 +0800

    Test of mocking a web service for the ISS Answers UCL-COMP0233-24-25/RSE-Classwork#18

--- test_times.py
+++ test_times.py
@@ -1,6 +1,8 @@
-from times import compute_overlap_time, time_range
+from times import compute_overlap_time, time_range, get_data, iss_passes
 import pytest
 import yaml
+from unittest.mock import patch
+import requests
 
 with open('fixture.yaml','r') as f:
     data = yaml.safe_load(f)
@@ -20,3 +22,31 @@ def test_overlap_cases(first_range,second_range,expected):
 def test_backward_time_range():
     with pytest.raises(ValueError):
         time_range('2010-10-02 10:00:00','2010-10-01 10:00:00')
+
+
+def test_iss_passes():
+
+    mock_response = {'passes': 
+                     [{'startAz': 247.04, 'startAzCompass': 'WSW', 'startEl': 12.26, 'startUTC': 1730172375, 
+                       'maxAz': 172.51, 'maxAzCompass': 'S', 'maxEl': 35.37, 'maxUTC': 1730172685, 'endAz': 95.2, 
+                       'endAzCompass': 'E', 'endEl': 0.38, 'endUTC': 1730172995, 'mag': 0.1, 
+                       'duration': 145, 'startVisibility': 1730172850}]}
+
+    with patch.object(requests,'get') as mock_get:
+        mock_get.return_value.json = lambda: mock_response
+
+        time_data = get_data(25544,41.702,-76.014,0,2,300)
+        mock_get.assert_called_with(
+        'https://api.n2yo.com/rest/v1/satellite/visualpasses/25544/56/0/0/5/50&apiKey=33Q884-HFUV8K-SCS3LG-55CU',
+        params={
+            'id': 25544,
+            'observer_lat': 41.702,
+            'observer_lng': -76.014,
+            'observer_alt': 0,
+            'days': 2,
+            'min_visibility': 300})
+        
+        result = iss_passes(time_data.json())
+        expected = [('2024-10-29 11:26:15', '2024-10-29 11:36:35')]
+
+        assert result == expected
--- times.py
+++ times.py
@@ -1,5 +1,6 @@
 import datetime
-
+import requests
+import json
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
@@ -30,3 +31,23 @@ def compute_overlap_time(range1, range2):
             if high > low:
                 overlap_time.append((low, high))
     return overlap_time
+
+
+def get_data(id,observer_lat, observer_lng, observer_alt, days,min_visibility):
+    base = 'https://api.n2yo.com/rest/v1/satellite/visualpasses/25544/56/0/0/5/50&apiKey=33Q884-HFUV8K-SCS3LG-55CU'
+    params=dict(id=id,observer_lat=observer_lat, observer_lng=observer_lng, observer_alt=observer_alt, days=days,min_visibility=min_visibility)
+    return requests.get(base,params=params)
+
+def iss_passes(data):
+    time_range = []
+    for item in data['passes']:
+        start = datetime.datetime.fromtimestamp(item['startUTC']).strftime("%Y-%m-%d %H:%M:%S")
+        end = datetime.datetime.fromtimestamp(item['endUTC']).strftime("%Y-%m-%d %H:%M:%S")
+        time_range.append((start,end))
+
+    return time_range
+
+data=get_data(25544,41.702,-76.014,0,2,300).text
+data = json.loads(data)
+
+print(data)
