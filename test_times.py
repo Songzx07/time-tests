01c69ee0156ea77c805d798f8365446e96d42eb9
AngelosZou <yutong.zou.24@ucl.ac.uk>
2024-10-28 10:57:12 +0000

    Fix bug of times and set some tests

--- test_times.py
+++ test_times.py
@@ -1,4 +1,5 @@
 from times import compute_overlap_time, time_range
+import pytest
 
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -7,3 +8,31 @@ def test_given_input():
     result = compute_overlap_time(large,short) 
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_no_overlap():
+    large = time_range("2008-01-12 10:00:00", "2009-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+
+    result = compute_overlap_time(large,short)
+    expected = []
+    assert result == expected
+
+def test_several_interval():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60*10)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 11:45:00", 2, 60)
+
+    result = compute_overlap_time(large,short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:55:00'), ('2010-01-12 11:05:00', '2010-01-12 11:07:00'), ('2010-01-12 11:08:00', '2010-01-12 11:45:00')]
+    assert result == expected
+
+def test_same_end_start():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60*10)
+    short = time_range("2010-01-12 12:00:00", "2010-01-13 12:00:00", 2, 60)
+
+    result = compute_overlap_time(large,short)
+    expected = []
+    assert result == expected
+
+def test_raise():
+    with pytest.raises(ValueError):
+        large = time_range("2010-01-12 10:00:00", "2009-01-12 12:00:00")
--- times.py
+++ times.py
@@ -1,23 +1,27 @@
 import datetime
+import time
 
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
-    start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
-    end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
-    d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
-    sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
-                  start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
-                 for i in range(number_of_intervals)]
-    return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
+    start_time_stamp = time.mktime(time.strptime(start_time,"%Y-%m-%d %H:%M:%S"))
+    end_time_stamp = time.mktime(time.strptime(end_time,"%Y-%m-%d %H:%M:%S"))
+    if end_time_stamp < start_time_stamp:
+        raise ValueError()
+    inter = ((end_time_stamp - start_time_stamp) - gap_between_intervals_s * (number_of_intervals-1))/number_of_intervals
+    ranges = [(start_time_stamp + i*inter + i*gap_between_intervals_s,
+               start_time_stamp + (i+1)*inter + i*gap_between_intervals_s) for i in range(number_of_intervals)]
+    return ranges
 
 
 def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
+            if end1 <= start2 or end2 <= start1:
+                continue
             low = max(start1, start2)
             high = min(end1, end2)
-            overlap_time.append((low, high))
+            overlap_time.append((time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(low)), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(high))))
     return overlap_time
 
 if __name__ == "__main__":
