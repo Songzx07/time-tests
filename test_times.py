ece7f141c64d034bf56aba9ea996a7a73d0f4824
Zhexuan <ucapzso@ucl.ac.uk>
2024-10-28 19:03:04 +0800

    Add backward time test

--- test_times.py
+++ test_times.py
@@ -32,3 +32,7 @@ def test_multintervals_each():
     result = compute_overlap_time(large,short) 
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:31:40'), ('2010-01-12 10:32:05', '2010-01-12 10:33:45'), ('2010-01-12 10:34:10', '2010-01-12 10:35:50'), ('2010-01-12 10:36:15', '2010-01-12 10:37:00'), ('2010-01-12 10:38:20', '2010-01-12 10:40:00')]
     assert result == expected
+
+def test_backward_time_range():
+    with raises(ValueError):
+        time_range('2010-10-02 10:00:00','2010-10-01 10:00:00')
