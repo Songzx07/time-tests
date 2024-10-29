32e31314bbf3b2b7176c1a4721e0daef32d362dc
Zhexuan <ucapzso@ucl.ac.uk>
2024-10-29 23:23:09 +0000

    Reformat codes with pep8 format

--- test_times.py
+++ test_times.py
@@ -4,48 +4,50 @@ import yaml
 from unittest.mock import patch
 import requests
 
-with open('fixture.yaml','r') as f:
+with open('fixture.yaml', 'r') as f:
     data = yaml.safe_load(f)
 
-test_data=[]
+test_data = []
 for case in data.values():
-    element = (time_range(case['time_range_1']['start'], case['time_range_1']['end'],int(case['time_range_1']['range_num']),int(case['time_range_1']['range_gap'])),
-               time_range(case['time_range_2']['start'], case['time_range_2']['end'],int(case['time_range_2']['range_num']),int(case['time_range_2']['range_gap'])),
+    element = (time_range(case['time_range_1']['start'], case['time_range_1']['end'], int(case['time_range_1']['range_num']), int(case['time_range_1']['range_gap'])),
+               time_range(case['time_range_2']['start'], case['time_range_2']['end'], int(
+                   case['time_range_2']['range_num']), int(case['time_range_2']['range_gap'])),
                [tuple(expected) for expected in case['expected']])
     test_data.append(element)
 
-@pytest.mark.parametrize('first_range,second_range,expected',test_data)
-def test_overlap_cases(first_range,second_range,expected):
-    assert compute_overlap_time(first_range,second_range)==expected
+
+@pytest.mark.parametrize('first_range,second_range,expected', test_data)
+def test_overlap_cases(first_range, second_range, expected):
+    assert compute_overlap_time(first_range, second_range) == expected
 
 
 def test_backward_time_range():
     with pytest.raises(ValueError):
-        time_range('2010-10-02 10:00:00','2010-10-01 10:00:00')
+        time_range('2010-10-02 10:00:00', '2010-10-01 10:00:00')
 
 
 def test_iss_passes():
 
-    mock_response = {'passes': 
-                     [{'startAz': 247.04, 'startAzCompass': 'WSW', 'startEl': 12.26, 'startUTC': 1730172375, 
-                       'maxAz': 172.51, 'maxAzCompass': 'S', 'maxEl': 35.37, 'maxUTC': 1730172685, 'endAz': 95.2, 
-                       'endAzCompass': 'E', 'endEl': 0.38, 'endUTC': 1730172995, 'mag': 0.1, 
+    mock_response = {'passes':
+                     [{'startAz': 247.04, 'startAzCompass': 'WSW', 'startEl': 12.26, 'startUTC': 1730172375,
+                       'maxAz': 172.51, 'maxAzCompass': 'S', 'maxEl': 35.37, 'maxUTC': 1730172685, 'endAz': 95.2,
+                       'endAzCompass': 'E', 'endEl': 0.38, 'endUTC': 1730172995, 'mag': 0.1,
                        'duration': 145, 'startVisibility': 1730172850}]}
 
-    with patch.object(requests,'get') as mock_get:
+    with patch.object(requests, 'get') as mock_get:
         mock_get.return_value.json = lambda: mock_response
 
-        time_data = get_data(25544,41.702,-76.014,0,2,300)
+        time_data = get_data(25544, 41.702, -76.014, 0, 2, 300)
         mock_get.assert_called_with(
-        'https://api.n2yo.com/rest/v1/satellite/visualpasses/25544/56/0/0/5/50&apiKey=33Q884-HFUV8K-SCS3LG-55CU',
-        params={
-            'id': 25544,
-            'observer_lat': 41.702,
-            'observer_lng': -76.014,
-            'observer_alt': 0,
-            'days': 2,
-            'min_visibility': 300})
-        
+            'https://api.n2yo.com/rest/v1/satellite/visualpasses/25544/56/0/0/5/50&apiKey=33Q884-HFUV8K-SCS3LG-55CU',
+            params={
+                'id': 25544,
+                'observer_lat': 41.702,
+                'observer_lng': -76.014,
+                'observer_alt': 0,
+                'days': 2,
+                'min_visibility': 300})
+
         result = iss_passes(time_data.json())
         expected = [('2024-10-29 03:26:15', '2024-10-29 03:36:35')]
 
--- times.py
+++ times.py
@@ -2,10 +2,12 @@ import datetime
 import requests
 import json
 
+
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
-    d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
+    d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + \
+        gap_between_intervals_s * (1 / number_of_intervals - 1)
     if d < 0:
         raise ValueError('Time range d='+str(d)+'is negative')
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
@@ -16,7 +18,7 @@ def time_range(start_time, end_time, number_of_intervals=1, gap_between_interval
 
 def compute_overlap_time(range1, range2):
     overlap_time = []
-    
+
     if range1[-1][1] == range2[0][0]:
         overlap_time.append(tuple([range1[-1][1]]))
         return overlap_time
@@ -33,16 +35,20 @@ def compute_overlap_time(range1, range2):
     return overlap_time
 
 
-def get_data(id,observer_lat, observer_lng, observer_alt, days,min_visibility):
+def get_data(id, observer_lat, observer_lng, observer_alt, days, min_visibility):
     base = 'https://api.n2yo.com/rest/v1/satellite/visualpasses/25544/56/0/0/5/50&apiKey=33Q884-HFUV8K-SCS3LG-55CU'
-    params=dict(id=id,observer_lat=observer_lat, observer_lng=observer_lng, observer_alt=observer_alt, days=days,min_visibility=min_visibility)
-    return requests.get(base,params=params)
+    params = dict(id=id, observer_lat=observer_lat, observer_lng=observer_lng,
+                  observer_alt=observer_alt, days=days, min_visibility=min_visibility)
+    return requests.get(base, params=params)
+
 
 def iss_passes(data):
     time_range = []
     for item in data['passes']:
-        start = datetime.datetime.fromtimestamp(item['startUTC']).strftime("%Y-%m-%d %H:%M:%S")
-        end = datetime.datetime.fromtimestamp(item['endUTC']).strftime("%Y-%m-%d %H:%M:%S")
-        time_range.append((start,end))
+        start = datetime.datetime.fromtimestamp(
+            item['startUTC']).strftime("%Y-%m-%d %H:%M:%S")
+        end = datetime.datetime.fromtimestamp(
+            item['endUTC']).strftime("%Y-%m-%d %H:%M:%S")
+        time_range.append((start, end))
 
     return time_range
